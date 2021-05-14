c417ddb3f9fff47ad373a0adb69c4cdae7d0fb27
Jeroen van Bart <77612186+jeroenvanbart@users.noreply.github.com>
2021-05-14 22:08:06 +0200

    changed routes

--- models/user-model.js
+++ models/user-model.js
@@ -1,22 +1,27 @@
-const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  username: String,
-  email: String,
-  password: String,
-  profileImg: String,
-  reviews: [{type: Schema.Types.ObjectId, ref: 'Review'}],
-  usertype: {
-    type: String, 
-    enum: ["Pet Owner", "Pet Sitter"], 
-    default:"Pet Owner",
+const userSchema = new Schema(
+  {
+    username: String,
+    email: String,
+    password: String,
+    profileImg: {
+      type: String,
+      default:
+        "https://res.cloudinary.com/defncxxxy/image/upload/v1620846241/Pawtell/file_yot0h7.jpg",
     },
+    reviews: [{ type: Schema.Types.ObjectId, ref: "Review" }],
+    usertype: {
+      type: String,
+      enum: ["Pet Owner", "Pet Sitter"],
+      default: "Pet Owner",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-}, 
-{
-  timestamps: true
-});
-
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 module.exports = User;
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -5,21 +5,20 @@ const router = express.Router();
 
 const User = require("../models/user-model");
 
-router.get("/profile", (req, res, next) => {
-  User.find().then((data) => {
-    res.status(200).json(data);
-    return;
-  }).catch((err) => {
-    res.status(500).json(err);
-    next(err);
-  });
-});
+// router.get("/profile", (req, res, next) => {
+//   User.find().then((data) => {
+//     res.status(200).json(data);
+//     return;
+//   }).catch((err) => {
+//     res.status(500).json(err);
+//     next(err);
+//   });
+// });
 
 router.get("/profile/:id", (req, res, next) => {
   const { id } = req.params;
   User.findById(id)
   .then((user) => {
-    console.log(user)
     res.status(200).json(user);
   })
   .catch((error) => {
