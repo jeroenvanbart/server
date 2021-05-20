be14f2fd32c08ad9ff40f46a28408fb3ce7a77e7
Jeroen van Bart <77612186+jeroenvanbart@users.noreply.github.com>
2021-05-20 12:38:28 +0200

    fixed few bugs

--- configs/cors.config.js
+++ configs/cors.config.js
@@ -6,6 +6,7 @@ module.exports = (incommingApp) => {
     incommingApp.use(
         cors({
             credentials: true,
+            preflightContinue: true,
             origin: [FRONTEND_ENDPOINT],
         })
     );
