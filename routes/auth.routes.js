7d81d2c71200a5fd41c1c7551df721f37820c191
Jeroen van Bart <77612186+jeroenvanbart@users.noreply.github.com>
2021-05-20 16:48:45 +0200

    fixed few bugs

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -11,23 +11,29 @@ const SALT_ROUNDS = 10;
 const User = require("../models/user-model");
 
 authRoutes.post("/signup", (req, res, next) => {
-    const { username, email, password, profileImg} = req.body;
-  
-    if (!username || !password || !email ) {
-      res.status(400).json({ message: "Fill out all fields" });
-      return;
-    }
-  
-    // make sure passwords are strong:
-    const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
-    if (!regex.test(password)) {
-      res.status(500).json({
-        message:
-          "Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter.",
+  const { username, email, password, profileImg } = req.body;
+
+  if (!username || !password || !email) {
+    res.status(400).json({ message: "Fill out all fields" });
+    return;
+  }
+
+  // make sure passwords are strong:
+  const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+  if (!regex.test(password)) {
+    res.status(500).json({
+      message:
+        "Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter.",
+    });
+    return;
+  }
+  User.findOne({ email }).then((userResult) => {
+    if (userResult) {
+      res.status(400).json({
+        message: "Email already exists",
       });
       return;
     }
-  
     bcrypt
       .genSalt(SALT_ROUNDS)
       .then((salt) => bcrypt.hash(password, salt))
@@ -41,9 +47,9 @@ authRoutes.post("/signup", (req, res, next) => {
       })
       .then((userFromDB) => {
         console.log("Newly created user is: ", userFromDB);
-  
+
         const { _id, username, createdAt, updatedAt } = userFromDB;
-  
+
         res.status(200).json({ _id, username, createdAt, updatedAt });
       })
       .catch((error) => {
@@ -57,49 +63,50 @@ authRoutes.post("/signup", (req, res, next) => {
         }
       });
   });
-  
-  authRoutes.post("/login", (req, res, next) => {
-    passport.authenticate("local", (err, theUser, failureDetails) => {
-      if (err) {
-        res
-          .status(500)
-          .json({ message: "Something went wrong authenticating user" });
-        return;
-      }
-  
-      if (!theUser) {
-        // "failureDetails" contains the error messages
-        // from our logic in "LocalStrategy" { message: '...' }.
-        res.status(401).json(failureDetails);
-        return;
-      }
-  
-      // save user in session
-      req.login(theUser, (err) => {
-        if (err) {
-          res.status(500).json({ message: "Session save went bad." });
-          return;
-        }
-  
-        // We are now logged in (that's why we can also send req.user)
-        res.status(200).json(theUser);
-      });
-    })(req, res, next);
-  });
-  
-  authRoutes.post("/logout", (req, res, next) => {
-    // req.logout() is defined by passport
-    req.logout();
-    res.status(200).json({ message: "Log out success!" });
-  });
-  
-  authRoutes.get("/loggedin", (req, res, next) => {
-    // req.isAuthenticated() is defined by passport
-    if (req.isAuthenticated()) {
-      res.status(200).json(req.user);
+});
+
+authRoutes.post("/login", (req, res, next) => {
+  passport.authenticate("local", (err, theUser, failureDetails) => {
+    if (err) {
+      res
+        .status(500)
+        .json({ message: "Something went wrong authenticating user" });
       return;
     }
-    res.status(403).json({ message: "Unauthorized" });
-  });
-  
+
+    if (!theUser) {
+      // "failureDetails" contains the error messages
+      // from our logic in "LocalStrategy" { message: '...' }.
+      res.status(401).json(failureDetails);
+      return;
+    }
+
+    // save user in session
+    req.login(theUser, (err) => {
+      if (err) {
+        res.status(500).json({ message: "Session save went bad." });
+        return;
+      }
+
+      // We are now logged in (that's why we can also send req.user)
+      res.status(200).json(theUser);
+    });
+  })(req, res, next);
+});
+
+authRoutes.post("/logout", (req, res, next) => {
+  // req.logout() is defined by passport
+  req.logout();
+  res.status(200).json({ message: "Log out success!" });
+});
+
+authRoutes.get("/loggedin", (req, res, next) => {
+  // req.isAuthenticated() is defined by passport
+  if (req.isAuthenticated()) {
+    res.status(200).json(req.user);
+    return;
+  }
+  res.status(403).json({ message: "Unauthorized" });
+});
+
 module.exports = authRoutes;
