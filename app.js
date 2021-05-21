2d61462dff7732359d21cface2c8bea662ac0e1e
Jeroen van Bart <77612186+jeroenvanbart@users.noreply.github.com>
2021-05-21 10:11:12 +0200

    fixed few bugs

--- app.js
+++ app.js
@@ -9,8 +9,6 @@ const passport = require("passport");
 require("./configs/passport");
 require("./configs/db.config");
 
-
-
 const app = express();
 
 app.use(logger("dev"));
@@ -18,9 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-
-
-require("./configs/session.config")(app)
+require("./configs/session.config")(app);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -29,14 +25,11 @@ app.locals.title = "Express - Generated with IronGenerator";
 
 require("./configs/cors.config")(app);
 
-
-
 app.use("/", require("./routes/index"));
-app.use("/api", require("./routes/auth.routes"));
-app.use("/api", require("./routes/upload.routes"));
-app.use("/api", require("./routes/user.routes"));
-app.use("/api", require("./routes/pet.routes"));
-app.use("/api", require("./routes/date.routes"));
-
+app.use("/", require("./routes/auth.routes"));
+app.use("/", require("./routes/upload.routes"));
+app.use("/", require("./routes/user.routes"));
+app.use("/", require("./routes/pet.routes"));
+app.use("/", require("./routes/date.routes"));
 
 module.exports = app;
